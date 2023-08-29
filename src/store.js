20de2c94edf731281d8cfd4be9ba702202840a6d
Aleksandar Ratkov <alexandar.ratkov@gmail.com>
2023-08-29 19:49:39 +0200

    Add redux persist and save cart state

--- src/store.js
+++ src/store.js
@@ -1,11 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit'
-
 import productsReducer from './features/products/productsSlice'
 import cartReducer from './features/cart/cartSlice'
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 
-export default configureStore({
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
+
+export const store = configureStore({
   reducer: {
     products: productsReducer,
-    cart: cartReducer,
+    cart: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store)
